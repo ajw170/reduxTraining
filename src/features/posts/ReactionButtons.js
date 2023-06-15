7ee2e2767ea02c6fe3d143cfe310756dfdf4e0eb
woodandr <andrew.j.wood@adp.com>
2023-06-15 16:55:07 -0400

    fixed bug

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -12,7 +12,7 @@ const reactionEmoji = {
 
 export const ReactionButtons = ({ post }) => {
   const dispatch = useDispatch();
-  const reactionButtons = Object.entries(reactionEmoji ?? []).map(([name, emoji]) => {
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
       <button key={name} type="button" className="muted-button reaction-button"
       onClick={() => dispatch(reactionAdded({ postId: post.id, reaction: name}))}>
